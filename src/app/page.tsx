8fe2ae6ed7e760ff8c4acdeaf909f27f6f759907
yadi-soe <yadisoe122734@gmail.com>
2026-01-11 16:06:10 +0200

    feat : page for main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,47 @@
+// import Image from "next/image";
+
+import { Disc2Icon, MoveUpRight } from "lucide-react";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="mx-auto max-w-4xl px-8 py-11">
+      <div className="flex justify-end gap-5">
+        <div className="border border-black px-5 py-2 inline-block text-sm">
+          Log in
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="border border-black px-5 py-2 inline-block text-sm">
+          Register
+        </div>
+      </div>
+      <main className=" grid grid-cols-2 border border-black w-auto mt-10">
+        <div className="flex flex-col items-start p-20">
+          <h3 className="text-xs mb-4">Let&apos;s get started</h3>
+          <p className="text-gray-700 text-xs mb-7">
+            Laravel has an incredibly rich ecosystem.
+            We suggest starting with the following.
+          </p>
+          <div className="flex flex-col mb-8 gap-5">
+              <div className="flex gap-5 text-xs mb-5">
+              <Disc2Icon className="size-4 text-gray-400"/>
+              <p>Read the <span className="text-orange-600 underline">
+                  documentation<MoveUpRight className="size-4 text-orange-600"/>
+                </span>
+              </p>
+            </div>
+            <div className="flex gap-5 text-xs">
+              <Disc2Icon className="size-4 text-gray-400"/>
+              <p>Watch video tutorials at <span className="text-orange-600 underline">
+                Laracast
+                </span><MoveUpRight className="size-4 text-orange-600"/>
+              </p>
+            </div>
+          </div>
+          <button className="bg-black text-white px-7 py-2 text-sm">Deploy now</button>
+        </div>
+        <div className="relative h-auto w-full">
+          <Image src="/dTOMI3nTXkkRLlAPSTlMPakchMPgst2khW74zLIS.png" alt="Laravel" fill className="object-cover"/>
         </div>
       </main>
     </div>
